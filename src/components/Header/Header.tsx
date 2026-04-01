cf43c0009efde4fdcca285d6ac96566e766f47d4
Vladimer Gabisonia <vakogabo44@gmail.com>
2026-04-01 23:09:55 +0400

    feat: add Header component with styles

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,17 +1,22 @@
 import React from "react";
+
 import Button from "../Button/Button";
+
+import "./Header.scss";
 
 const Header: React.FC = () => {
   return (
     <header>
       <img src="/images/logo.png" alt="logo" />
-
       <div>
-        <div>
+        <div className="browse-courses-box">
           <img src="/images/three-stars.png" alt="three stars" />
           <span>Browse Courses</span>
         </div>
-        <Button text="Log In" />
+        <div className="buttons-box">
+          <Button text="Log In" />
+          <Button text="Sign Up" purpleBackground />
+        </div>
       </div>
     </header>
   );
